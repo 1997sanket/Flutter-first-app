7254cb3927fe467b93edb2806fe048db793f7a35
Sanket Kamble <1997sanket.k@gmail.com>
2021-09-30 03:31:34 +0530

    Converted MyApp class to a Stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  //Entry point
+  runApp(MyApp()); //Giving object of our App
 }
 
-class MyApp extends StatelessWidget {
+/* Extending Stateful because we wanted to change state(instance member) of 
+our widget */
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+//Create a new class for the changing instance members and our code shifts here
+class MyAppState extends State<MyApp> {
+  var questions = ["Your fav color ?", "Your fav movie ?"];
+  var index = 0;
+
+  //function which changes our state
   void answerQuestion() {
-    print("Button pressed");
+    setState(() =>
+        {index++}); //Change of state, needs to be done in "setState()" method
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      //Page of our app with basic design
       home: Scaffold(
         appBar: AppBar(title: Text("First App")),
+        //If we want to show data vertically
         body: Column(
           children: [
-            Text("Your fav color ?"),
+            Text(questions[index]),
             RaisedButton(child: Text("Green"), onPressed: answerQuestion),
             RaisedButton(child: Text("Blue"), onPressed: answerQuestion),
             RaisedButton(child: Text("Red"), onPressed: answerQuestion)
